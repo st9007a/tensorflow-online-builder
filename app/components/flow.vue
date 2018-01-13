f8090cda7b5e58e3b392ec742e7130908169abc4
uno <st9007a@gmail.com>
2018-01-13 11:12:10 +0800

    Let path as attribute in flow component

--- app/components/flow.vue
+++ app/components/flow.vue
@@ -1,10 +1,55 @@
 <template lang="pug">
-path.v-flow(d='M124 30 L150 30', stroke='black', stroke-width='5')
+path.v-flow(:d='getPath', stroke='black', stroke-width='5')
 </template>
 
 <script>
 export default {
   name: 'Flow',
+
+  props: ['startX', 'startY', 'endX', 'endY'],
+
+  created() {
+    this.$data.path.startX = this.startX
+    this.$data.path.startY = this.startY
+    this.$data.path.endX = this.endX
+    this.$data.path.endY = this.endY
+  },
+
+  computed: {
+    getPath() {
+      return `M${this.$data.path.startX} ${this.$data.path.startY} L${this.$data.path.endX} ${this.$data.path.endY}`
+    },
+  },
+
+  data() {
+    return {
+      path: {
+        startX: 0,
+        startY: 0,
+        endX: 0,
+        endY: 0,
+      }
+    }
+  },
+
+  watch: {
+    startX(val) {
+      this.$data.path.startX = val
+    },
+
+    startY(val) {
+      this.$data.path.startY = val
+    },
+
+    endX(val) {
+      this.$data.path.endX = val
+    },
+
+    endY(val) {
+      this.$data.path.endY = val
+    },
+  }
+
 }
 </script>
 
