acc93f3080f1ca572bbd3f80ffc7a71abd1c9cc7
uno <st9007a@gmail.com>
2018-01-20 16:33:48 +0800

    Remove production mode checking

--- app/app.js
+++ app/app.js
@@ -7,13 +7,6 @@ import Vue from 'vue'
 
 import App from './app.vue'
 
-
-if (process.env.NODE_ENV === 'production') {
-  Vue.config.devtools = false
-  Vue.config.debug = false
-  Vue.config.silent = true
-}
-
 const  app = new Vue({
   el: '#app',
   render: h => h(App),
